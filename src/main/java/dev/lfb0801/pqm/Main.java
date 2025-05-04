aedec5b9d945ff2685cdf7dcc79245e66a885167
Lloyd van Zaalen <lfb0801@gmail.com>
2025-05-04 23:02:02 +0200

    initial commit

---
+++ src/main/java/dev/lfb0801/pqm/Main.java
@@ -0,0 +1,11 @@
+package dev.lfb0801.pqm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String... args) throws Exception {
+        var context = SpringApplication.run(Main.class, args);
+    }
+}
